a323297a0199e759e967192c5a332d90c713d2ad
mweissen <miro.weissenberg@gmail.com>
2026-01-30 11:10:39 +0100

    reverted to 50 pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=60', mapResults)
+  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
   if (isLoading) {
     return <LoadingSpinner />
